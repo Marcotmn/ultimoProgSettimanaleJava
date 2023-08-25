adb0fe729309d5ab50b7841c6a889b946d43a8f0
Marcotmn <129113184+Marcotmn@users.noreply.github.com>
2023-08-25 10:30:35 +0200

    Add files via upload

---
+++ ultimoProgettoSettimanaleJava/src/test/java/marcotumminia/ultimoProgettoSettimanaleJava/UltimoProgettoSettimanaleJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package marcotumminia.ultimoProgettoSettimanaleJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimoProgettoSettimanaleJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
